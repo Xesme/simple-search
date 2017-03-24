c9814b22c44306ea03a65701b56a769c0a31467a
xa <xiaesmeamen@me.com>
2017-03-23 21:53:05 -0700

    add styling with SASS

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var doctorDisplay = function(image, name, gender, liscensed, specialty, bio){
-  $('#results').append("<li>" + name + '<br><img src="' + image + '"> <br>'+ "</li>");
+  $('#results').append("<div class='well'><li>" + '<img src="' + image + '"> <br><hr>'+ name + "<br><hr>Liscensed in: " + liscensed + "<br><hr>" + specialty + "<br><hr>Biography" + bio + "</li>");
 };
 
 $(document).ready(function(){
@@ -13,6 +13,6 @@ $(document).ready(function(){
     var locationInput = $('#location').val();
     var genderInput = $('#gender').val();
     newDoctor.search(ailmentInput, locationInput, genderInput, doctorDisplay);
+    $('#results').empty();
   });
-
 });
