39d2c22720b778d9203d8c6b2ba0f27f82c304e6
xa <xiaesmeamen@me.com>
2017-03-23 19:55:02 -0700

    define newDoctor (doctor-interface.js) make successful API call for 'headache'

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,16 @@
-var apiKey = require('./../.env').apiKey;
+var Doctor = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function(){
+  var newDoctor = new Doctor();
+
+  $('#search-form').submit(function(event){
+    event.preventDefault();
+    var ailmentInput = $('#ailment').val();
+    var specialtyInput = $('#specialty').val();
+    var locationInput = $('#location').val();
+    var genderInput = $('#gender').val();
+    newDoctor.search(ailmentInput, specialtyInput, locationInput, genderInput);
+    console.log(newDoctor);
+  });
+
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,21 @@
 var apiKey = require('./../.env').apiKey;
+
+function Doctor() {
+}
+
+Doctor.prototype.search = function(ailment, specialty, location, gender){
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&location=' + location + '&gender=' + gender + '&limit=10&user_key=' + apiKey).then(function(response){
+    console.log(response);
+    $('#results').append('<li>' + 'these are the doctors we found within your search' + "</li>");
+  });
+};
+
+exports.doctorModule = Doctor;
+
+
+
+// search conditions
+// ailment
+// specialty
+// location
+// gender
