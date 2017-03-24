3e4f9b167449ebd2a5904bc8399b9f5361c4b058
xa <xiaesmeamen@me.com>
2017-03-23 20:59:54 -0700

    create callback function and delete commented out code (doctor.js and doctor-interface.js)

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,9 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var doctorDisplay = function(image, name, gender, liscensed, specialty, bio){
+  $('#results').append("<li>" + name + '<br><img src="' + image + '"> <br>'+ "</li>");
+};
+
 $(document).ready(function(){
   var newDoctor = new Doctor();
 
@@ -9,7 +13,7 @@ $(document).ready(function(){
     var specialtyInput = $('#specialty').val();
     var locationInput = $('#location').val();
     var genderInput = $('#gender').val();
-    newDoctor.search(ailmentInput, specialtyInput, locationInput, genderInput);
+    newDoctor.search(ailmentInput, specialtyInput, locationInput, genderInput, doctorDisplay);
   });
 
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,15 +3,19 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.search = function(ailment, specialty, location, gender){
+Doctor.prototype.search = function(ailment, specialty, location, gender, doctorDisplay){
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&location=' + location + '&gender=' + gender + '&limit=10&user_key=' + apiKey).then(function(response){
     console.log(response);
     for(var x = 0; x <= 4; x++){
-      console.log(response.data[x].profile.first_name );
+      var image = response.data[x].profile.image_url;
+      var name = response.data[x].profile.first_name + " " + response.data[x].profile.last_name + " " +
+       response.data[x].profile.title;
+      var gender = response.data[x].profile.gender;
+      var liscensed = response.data[x].licenses[0].state;
+      var specialty = response.data[x].specialties[0].description;
+      var bio = response.data[0].profile.bio;
+      doctorDisplay(image, name, gender, liscensed, specialty, bio);
     }
-    // $('#results').append('<li>' + '<img sr="' + response.data[0].profile.img_url + '"/><br>' + 'name: ' + response.data[0].profile.first_name + " " +
-    // response.data[0].profile.last_name +  response.data[0].profile.title + "<br>" +
-    // "gender: " + response.data[0].profile.gender +  "<br>licensed in the state of : " + response.data[0].licenses[0].state + "<br>" + "specialties: " + response.data[0].specialties[0].description +  "<br> biography: " + response.data[0].profile.bio + "</li>");
   });
 };
 
