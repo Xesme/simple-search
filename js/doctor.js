4dc7f6ca238c0068b30b4749fbcf8686020c7b06
xa <xiaesmeamen@me.com>
2017-03-23 20:19:04 -0700

    update response results (doctor.js)

--- js/doctor.js
+++ js/doctor.js
@@ -6,7 +6,9 @@ function Doctor() {
 Doctor.prototype.search = function(ailment, specialty, location, gender){
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + ailment + '&location=' + location + '&gender=' + gender + '&limit=10&user_key=' + apiKey).then(function(response){
     console.log(response);
-    $('#results').append('<li>' + 'these are the doctors we found within your search' + "</li>");
+    $('#results').append('<li>' + '<img sr="' + response.data[0].profile.img_url + '"/><br>' + 'name: ' + response.data[0].profile.first_name + " " +
+    response.data[0].profile.last_name +  response.data[0].profile.title + "<br>" +
+    "gender: " + response.data[0].profile.gender +  "<br>licensed in the state of : " + response.data[0].licenses[0].state + "<br>" + "specialties: " + response.data[0].specialties[0].description +  "<br> biography: " + response.data[0].profile.bio + "</li>");
   });
 };
 
